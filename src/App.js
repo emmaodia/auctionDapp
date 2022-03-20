16e56377a1102b7c9f05e9f79367530e4626eead
emmaodia <odia.emma@gmail.com>
2022-03-20 01:27:55 +0100

    Set up provider and signer.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
+import { ethers, network } from 'hardhat';
 import './App.css';
 // import {Contract}  from 'abi';
+
+require('dotenv').config()
+
+const ALCHEMY_KEY = process.env.ALCHEMY_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const CONTRACT_ADDRESS = "0x43028d82134457Ad49Ca0f3af3cE9f1DB5e6B89A";
 
 console.log("Here");
 
 const Contract = require("./contracts/Auction.sol/Auction.json")
 
+const provider = new ethers.provider.AlchemyProvider(network="ropsten", ALCHEMY_KEY)
+const signer = new ethers.Wallet(PRIVATE_KEY, provider)
 
-console.log(Contract);
+const auction = new ethers.Contract(CONTRACT_ADDRESS, Contract.abi, signer)
+
+console.log(Contract.abi);
 console.log(Contract.contractName);
+
+async function main() {
+  const auctionEnd = await auction.auctionEnd();
+
+  console.log(auctionEnd);
+}
+
+main();
 
 function App() {
   return (
