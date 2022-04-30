11001ac8a5069fb198cce039d7581fb2bd1d3bee
emmaodia <odia.emma@gmail.com>
2022-04-30 17:00:18 +0100

    Connect MetaMask Wallet component.

---
+++ src/Components/ConnectWallet.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { useState } from 'react';
+import { ethers } from 'ethers';
+import Button from '@mui/material/Button';
+
+
+function ConnectWallet() {
+
+    const [connButtonText, setConnButtonText] = useState("Connect Wallet")
+    const [errorMessage, setErrorMessage] = useState(null);
+      const [defaultAccount, setDefaultAccount] = useState(null);
+      const [provider, setProvider] = useState(null);
+
+    const wallectConnection = () => {
+       
+        if (window.ethereum && defaultAccount == null) {
+            // set ethers provider
+            setProvider(new ethers.providers.Web3Provider(window.ethereum));
+            // connect to metamask
+            window.ethereum.request({ method: 'eth_requestAccounts'})
+            .then(result => {
+                setConnButtonText('Wallet Connected');
+                setDefaultAccount(result[0]);
+            })
+            .catch(error => {
+                setErrorMessage(error.message);
+            });
+        } else if (!window.ethereum){
+            console.log('Need to install MetaMask');
+            setErrorMessage('Please install MetaMask browser extension to interact');
+        }
+    }
+
+    const walletAccountChanged = (newAccount) => {
+        setDefaultAccount(newAccount)
+    }
+
+    window.ethereum.on('accountsChanged', walletAccountChanged);
+    
+    return (
+        <div>
+        <Button size="small" variant="outlined" onClick={wallectConnection}>{connButtonText}</Button>
+        <div>{errorMessage}{defaultAccount}</div>
+        </div>
+    )
+}
+
+export default ConnectWallet;
